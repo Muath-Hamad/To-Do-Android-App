c243658bd5147e6dab326af324b1df0ff38453ca
Muath-Hamad <muaath.jem@gmail.com>
2022-07-23 08:39:49 +0300

    initial design for Create_Event

--- app/src/main/java/com/training/calendar/Create_Event.java
+++ app/src/main/java/com/training/calendar/Create_Event.java
@@ -2,9 +2,15 @@ package com.training.calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Create_Event extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private TextView StartDate , EndDate ,StartTime , EndTime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
