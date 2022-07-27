99332d41214e48ace6e4919d35c98f3ba6fec8dc
Muath-Hamad <muaath.jem@gmail.com>
2022-07-27 11:50:07 +0300

    Add fab in main activity now works properly

--- app/src/main/java/com/training/calendar/MainActivity.java
+++ app/src/main/java/com/training/calendar/MainActivity.java
@@ -3,6 +3,7 @@ package com.training.calendar;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,20 +23,42 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+boolean isShown = false;
+FloatingActionButton addCateg , addEvent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addEvent = findViewById(R.id.Create_EventBTN);
+        addCateg = findViewById(R.id.Create_CategoryBTN);
+
     }
 
     public void AddTask(View v){
-        Intent h = new Intent(MainActivity.this, Create_Event.class);
-        startActivity(h);
+         if (isShown){
+            addCateg.setVisibility(View.INVISIBLE);
+            addEvent.setVisibility(View.INVISIBLE);
+            isShown = false;
+        }else{
+            addCateg.setVisibility(View.VISIBLE);
+            addEvent.setVisibility(View.VISIBLE);
+            isShown = true;
+        }
     }
     public void TaskView(View v){
         Intent h = new Intent(MainActivity.this , Task_View.class);
+        startActivity(h);
+    }
+
+    public void AddCategory(View view) {
+        Intent h = new Intent(MainActivity.this, Create_Category.class);
+        startActivity(h);
+    }
+
+
+    public void AddEvent(View view) {
+        Intent h = new Intent(MainActivity.this, Create_Event.class);
         startActivity(h);
     }
 }
